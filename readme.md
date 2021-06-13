770cef3b9e4e2101f2521b1f508d0c0c2cb8e694
demonick82 <demonick82@gmail.com>
2021-06-14 00:45:04 +0300

    0. Техническое задание. Аргегатор Java Вакансий. [#260357]

---
+++ readme.md
@@ -0,0 +1,3 @@
+Grabber (Парсер вакансий)
+
+https://www.travis-ci.com/demonick82/job4j_grabber.svg?branch=main
---
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -0,0 +1,7 @@
+package ru.job4j.grabber;
+
+public class Grabber {
+    public int sum() {
+        return 2 + 3;
+    }
+}
