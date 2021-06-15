dd59d84ce14ea800a23caadcb35e974dde472468
demonick82 <demonick82@gmail.com>
2021-06-16 00:31:58 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -9,20 +9,35 @@ import java.io.IOException;
 
 public class SqlRuParse {
     public static void main(String[] args) throws IOException {
-        new SqlRuParse().parse(5);
+        new SqlRuParse().parseList(5);
     }
 
-    public void parse(int pages) throws IOException {
+    public void parseList(int pages) throws IOException {
         for (int i = 1; i <= pages; i++) {
             Document doc = Jsoup.connect(String.format("https://www.sql.ru/forum/job-offers/%d", i)).get();
             Elements row = doc.select(".postslisttopic");
             for (Element td : row) {
                 Element href = td.child(0);
                 Element date = td.parent().child(5);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                System.out.println(date.text());
+                String link = href.attr("href");
+                String name = href.text();
+                String dateText = date.text();
+                System.out.println(link);
+                System.out.println(name);
+                System.out.println(dateText);
+                parseLink(link);
+                System.out.println();
             }
         }
+    }
+
+    public void parseLink(String link) throws IOException {
+        Document doc = Jsoup.connect(link).get();
+        Elements row = doc.select(".msgBody");
+        Elements footer = doc.select(".msgFooter");
+        String postDetail = row.get(1).text();
+        String postDate = footer.first().ownText().replace(" [] |", "");
+        System.out.println(postDetail);
+        System.out.println(postDate);
     }
 }
