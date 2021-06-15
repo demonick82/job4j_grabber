610aab5dedeb28053fbd5721e885a6a6f00fa0f2
demonick82 <demonick82@gmail.com>
2021-06-15 14:17:46 +0300

    2.2. Модель данных - Post. [#285211]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -34,13 +34,17 @@ public class Post {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Post post = (Post) o;
-        return Objects.equals(postName, post.postName) &&
-                Objects.equals(postText, post.postText) &&
-                Objects.equals(postLink, post.postLink) &&
-                Objects.equals(postCreated, post.postCreated);
+        return Objects.equals(postName, post.postName)
+                && Objects.equals(postText, post.postText)
+                && Objects.equals(postLink, post.postLink)
+                && Objects.equals(postCreated, post.postCreated);
     }
 
     @Override
@@ -50,11 +54,11 @@ public class Post {
 
     @Override
     public String toString() {
-        return "Post{" +
-                "postName='" + postName + '\'' +
-                ", postText='" + postText + '\'' +
-                ", postLink='" + postLink + '\'' +
-                ", postCreated=" + postCreated +
-                '}';
+        return "Post{"
+                + "postName='" + postName + '\''
+                + ", postText='" + postText + '\''
+                + ", postLink='" + postLink + '\''
+                + ", postCreated=" + postCreated
+                + '}';
     }
 }
